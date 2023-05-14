8266a1b81d91a7c5beed408e9d188456efb35c52
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2023-05-14 23:54:57 +1000

    added the post route and successfully send the post request along with json

--- src/routers/taskRouters.js
+++ src/routers/taskRouters.js
@@ -10,60 +10,35 @@ let fakeDB = [
 
 // console.log(router);
 
-router.get("/", (req, res, next) => {
+router.get("/:_id?", (req, res, next) => {
   //   console.log("get");
   const { _id } = req.params;
   console.log(_id);
 
   let data = fakeDB;
 
-  //   if (_id) {
-  //     data = fakeDB.filter((item) => item._id === +_id);
-  //   }
+  try {
+    res.json({
+      status: "success",
+      message: "get route",
+      data,
+    });
+  } catch (error) {
+    error.status = 500;
+    next(error);
+  }
+});
 
-  res.json({
-    status: "success",
-    message: "get route",
-    data,
-  });
+router.post("/", (req, res, next) => {
+  try {
+    res.json({
+      status: "success",
+      message: "post route",
+    });
+  } catch (error) {
+    error.status = 500;
+    next(error);
+  }
 });
 
 export default router;
-
-// import express from "express";
-
-// let fakeDB = [
-//   { _id: 1, task: "dancing", hr: 1 },
-//   { _id: 2, task: "singing", hr: 2 },
-//   { _id: 3, task: "fucking", hr: 100 },
-// ];
-// const router = express.Router();
-
-// router.get("/", (req, res, next) => {
-//   let data = fakeDB;
-
-//   try {
-//     res.json({
-//       status: "success",
-//       message: "you hit the get root route",
-//       data,
-//     });
-//   } catch (error) {
-//     error.status = 500;
-//     next(error);
-//   }
-// });
-
-// router.post("/", (req, res, next) => {
-//   try {
-//     res.json({
-//       status: "success",
-//       message: "you hit the post root route",
-//     });
-//   } catch (error) {
-//     error.status = 500;
-//     next(error);
-//   }
-// });
-
-// export default router;
